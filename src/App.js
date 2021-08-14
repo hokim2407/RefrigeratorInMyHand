f70aa5130466a2771d6de74075305e1879f88ce0
kypark7 <minequinto2@gmail.com>
2021-08-14 16:57:37 +0900

    3rd page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import MainPage from "./mainPage";
 import SaveEnv from "./saveEnv";
+import Choice from "./choice";
 import CheckMine from "./checkMine";
 import FillPage from "./fillPage";
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Route path="/" component={MainPage} exact />
       <Route path="/saveEnv" component={SaveEnv} />
+      <Route path="/choice" component={Choice} />
       <Route path="/checkMine" component={CheckMine} />
       <Route path="/fill" component={FillPage} />
     </div>
