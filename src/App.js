4c1888c7657240bb7c0b35278605815f79629143
hokim <67669579+hokim2407@users.noreply.github.com>
2021-08-15 00:47:48 +0900

    Revert "#6 page"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import MainPage from "./mainPage";
 import SaveEnv from "./saveEnv";
+import Choice from "./choice";
 import CheckMine from "./checkMine";
 import FillPage from "./fillPage";
 import Result from "./result";
@@ -11,6 +12,7 @@ function App() {
     <div className="App">
       <Route path="/" component={MainPage} exact />
       <Route path="/saveEnv" component={SaveEnv} />
+      <Route path="/choice" component={Choice} />
       <Route path="/checkMine" component={CheckMine} />
       <Route path="/fill" component={FillPage} />
       <Route path="/result" component={Result} />
