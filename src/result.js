e65adf41871e0e10da4c07bc0eb63b0ce96be11e
hokim <teamgoodaloe@gmail.com>
2021-08-15 01:02:16 +0900

    fix: fix warning

--- src/result.js
+++ src/result.js
@@ -49,8 +49,8 @@ function Result() {
     if (image[checkType] === "./icon/imo-good.png") existence = "없습니다";
   });
 
-  if (checkType == 0) var Type = "상온";
-  else if (checkType == 1) Type = "냉장";
+  if (checkType === 0) var Type = "상온";
+  else if (checkType === 1) Type = "냉장";
   else Type = "냉동";
 
   console.log(Type);
@@ -87,7 +87,77 @@ function Result() {
     }
   });
 
-  return <div className="top-block"></div>;
+  return (
+    <div className="top-block">
+      <div className="background fill-background">
+        <div className="dark-background vertical-flex-box ">
+          <div
+            className="transparent-box"
+            style={{ top: "0", height: "100%", padding: "0" }}
+          >
+            <div
+              style={{
+                position: "relative",
+                align: "left",
+              }}
+            >
+              <p
+                className="pb1"
+                style={{
+                  color: "white",
+                  position: "relative",
+                }}
+              >
+                {Type}
+              </p>
+            </div>
+            <div
+              style={{
+                paddingBottom: "3vw",
+                textAlign: "left",
+                position: "relative",
+                align: "left",
+                backgroundColor: "rgba(255, 255, 255, 0.3)",
+              }}
+            >
+              <div style={{}}>
+                <p
+                  id="answer"
+                  className="h2 button-text"
+                  style={{
+                    margin: "0",
+                    fontSize: "min(5vw, 4rem)",
+                    paddingBlockStart: "3vw",
+                    color: "white",
+                    textDecoration: "underline",
+                    textUnderlinePosition: "under",
+                  }}
+                >
+                  <img
+                    className="button-icon"
+                    src={`${image[checkType]}`}
+                    alt="아이콘"
+                  ></img>
+                  유통기한 임박 제품이 {existence}
+                </p>
+                <ul>
+                  <li>
+                    <ul id="item-list" style={{ marginBlockEnd: "0" }}></ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div>
+              <ul
+                id="item-lists"
+                style={{ padding: "3vh 0 ", marginBottom: "0" }}
+              ></ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Result;
